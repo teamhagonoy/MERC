1be8cefcd818228c7d105fe64d5f7c0f7153608d
teamhagonoy <jadetormis@g.cjc.edu.ph>
2025-06-08 13:17:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 // Enable horizontal scrolling with mouse wheel for navigation
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
   const amenityList = document.getElementById('amenity-list');
-  
-  amenityList.addEventListener('wheel', function(e) {
+
+  amenityList.addEventListener('wheel', function (e) {
     e.preventDefault();
     amenityList.scrollLeft += e.deltaY;
   });
