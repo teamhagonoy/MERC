4e9a01a15ed3a86fcee450bccf8c9a8287f64bda
teamhagonoy <jadetormis@g.cjc.edu.ph>
2025-06-08 12:07:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("menu-button").onclick = () => {
+  const list = document.getElementById("amenity-list");
+  list.style.display = list.style.display === "none" ? "block" : "none";
+};
