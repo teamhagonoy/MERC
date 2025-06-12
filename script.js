25a8282c53828e2cd64228e73b95e579f94bb925
teamhagonoy <jadetormis@g.cjc.edu.ph>
2025-06-12 12:21:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const focusImage = document.getElementById('focusImage');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > window.innerHeight * 0.3) {
-      focusImage.classList.add('show');
-    } else {
-      focusImage.classList.remove('show');
-    }
-  });
-});
