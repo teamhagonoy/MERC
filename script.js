a54a533022e65d96ad22112be4464cc03a49abcc
teamhagonoy <jadetormis@g.cjc.edu.ph>
2025-06-12 13:19:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hero = document.querySelector('.hero-overlay');
+  console.log("Hero overlay loaded:", hero);
 
   window.addEventListener('scroll', () => {
+    console.log("ScrollY:", window.scrollY);
+
     if (window.scrollY > 50) {
       hero.classList.add('shrink');
     } else {
