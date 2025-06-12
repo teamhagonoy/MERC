496748b3e4be7cbc5277026318c97fc3d4341821
teamhagonoy <jadetormis@g.cjc.edu.ph>
2025-06-12 12:44:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.querySelector('.hero-overlay');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      hero.classList.add('shrink');
+    } else {
+      hero.classList.remove('shrink');
+    }
+  });
+});
 
